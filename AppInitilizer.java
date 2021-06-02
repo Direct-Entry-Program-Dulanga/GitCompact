4790a7d6db4f7a7a325e005581855b8f90240282
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-06-02 16:00:31 +0530

    Add new print Commit

---
+++ AppInitilizer.java
@@ -0,0 +1,7 @@
+public class AppInitializer{
+    public public static void main(String args[]) {
+        System.out.println("-------------------------------");
+        System.out.println("Add New Branch");
+        System.out.println("-------------------------------");
+    }
+}
